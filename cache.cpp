5c4e02e63eae5bf5105119a1151948ac02cdca6d
ivan <panferov.ia@phystech.edu>
2021-09-22 00:52:36 +0300

    Released CASE IV

--- cache.cpp
+++ cache.cpp
@@ -2,11 +2,26 @@
 #include <unordered_map>
 #include <iostream>
 
+#define InsertPage(id)										 \
+{															 \
+	id.push_front(page);									 \
+	hash_table.insert(pair<Page, PageIt<Page>>				 \
+						  (page, {id.begin(), ListId::id})); \
+															 \
+}															 \
+
+#define RemovePage(id)		\
+{							\
+	hash_table.erase(page);	\
+	id.pop_back();			\
+}							\
+
 using namespace std;
+using page = int;
 
 enum class ListId
 {
-	Null, T1, T2, B1, B2
+	T1, T2, B1, B2
 };
 
 template <typename Page>
@@ -15,10 +30,10 @@ class PageIt
 	using ListIt = typename list<Page>::iterator;
 
 	ListIt it;
-	ListId num;
+	ListId id;
 
 public:
-	PageIt(ListIt it, ListId num): it(it), num(num) {};
+	PageIt(ListIt it, ListId id): it(it), id(id) {};
 };
 
 template <typename Page>
@@ -26,53 +41,54 @@ class ARC
 {
 	unordered_map <Page, PageIt<Page>> hash_table;
 	list<Page> T1, T2, B1, B2;
-	size_t size;
-	int adapt_param;
-	int hits;
+	size_t size, adapt_param, hits;
 
 public:
-//if remove from B1, B2 ==>> remove from table
-//auto iterator = list.find(key)
-//size(T1 + B1) === size(T2 + B2) === size
 	ARC(int sz): size(sz), adapt_param(0), hits(0) {};
 
-	void PageInsert(Page page, ListId id)
+	void CacheInsert(const Page& page)
 	{
-		T1.push_front(page);
+		if (hash_table.find(page) != hash_table.cend())
+		{
 
-		hash_table.insert(pair<Page, PageIt<Page>>
-							  (page, {T1.begin(), ListId::T1}));
-/* 		{
-			auto iter = hash_table.find(page);
-
-			if(iter != hash_table.end())
-				cout << "Found" << endl;
-			else
-				cout << "Not Found" << endl;
 		}
- */	
+		else		//CASE IV//
+		{
+			if (GetL1Size() == size)
+			{
+				if (T1.size() < size)
+				{
+					RemovePage(B1);
+					Replace();
+				}
+				else RemovePage(T1);
+			}
+			else if (GetL1Size() < size && GetLSize() >= size)
+			{
+				if (GetLSize() == 2 * size) RemovePage(B1);
+				Replace();
+			}
+
+			InsertPage(T1);
+		}
 	}
 
-	void PageRemove(Page page, ListId id)
-	{
-		hash_table.erase(page);
-		T1.pop_back();
-	}
-	
 	void Replace()
 	{
 
 	}
+
+	size_t GetHits  () const { return hits; }
+	size_t GetL1Size() const { return T1.size()   + B1.size();   }
+	size_t GetL2Size() const { return T2.size()   + B2.size();   }
+	size_t GetLSize () const { return GetL1Size() + GetL2Size(); }
+
+//size(T1 + B1) === size(T2 + B2) === size
 };
 
 int main()
 {
-	ARC<int> cache(5);
-
-//	list<int> T1;
-
-	cache.PageInsert(775, ListId::T1);
-	cache.PageRemove(775, ListId::T1);
+	ARC<page> cache {5};
 
     return 0;
 }
